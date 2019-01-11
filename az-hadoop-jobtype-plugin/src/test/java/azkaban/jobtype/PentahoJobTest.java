43947a7b971f704d904d77434bb757c554bdbccb
Ismail Simsek <ismail.simsek@ppro.com>
2019-01-11 18:14:24 +0100

    adding test class2

--- az-hadoop-jobtype-plugin/src/test/java/azkaban/jobtype/PentahoJobTest.java
+++ az-hadoop-jobtype-plugin/src/test/java/azkaban/jobtype/PentahoJobTest.java
@@ -6,17 +6,19 @@ import azkaban.utils.Props;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+import azkaban.test;
 
 public class PentahoJobTest {
 
-  Props testSysProps = new Props();
-  Props testJobProps = new Props();
+  Props testSysProps = AllJobExecutorTests.setUpCommonProps();
+  Props testJobProps = AllJobExecutorTests.setUpCommonProps();
 
   private Props testProps;
   Logger logger = Logger.getRootLogger();
 
   @Before
   public void beforeMethod() throws Exception {
+    azkaban.test.Utils.initServiceProvider();
   }
 
 
@@ -33,7 +35,7 @@ public class PentahoJobTest {
     testJobProps.put("pentaho.data_integration_root_dir", "src/test/resources/pentaho/data-integration");
     testJobProps.put("pentaho.file", "src/test/resources/pentaho/etl/parameterized_job.kjb");
     testJobProps.put("pentaho.param.level", "Detailed");
-    testProps.put("pentaho.param.DEMO_PARAMETER_1", "Value of Pentaho ETL DEMO_PARAMETER_1!");
+    testJobProps.put("pentaho.param.DEMO_PARAMETER_1", "Value of Pentaho ETL DEMO_PARAMETER_1!");
 
     PentahoJob job = new PentahoJob("-1",testSysProps,testJobProps,logger);
     logger.info(job.createCommandLine());
